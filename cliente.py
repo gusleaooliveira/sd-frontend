00d4b011ccf6bd03dd404ab0fd1d56a60b4721e7
Gustavo Leão Nogueira de Oliveira <gus.leaono@gmail.com>
2021-07-14 09:51:19 -0300

    concluido

--- cliente.py
+++ cliente.py
@@ -1,14 +1,11 @@
 import web
 import terminal
-import sdthread
 
 if __name__ == '__main__':
     opcao=input('Opção [j/c/t]: ').lower()[0]
 
     if opcao == 'j':
         web.socket.run(web.app, debug=True)
-    if opcao == 't':
-        sdthread.app.mainloop()
     if opcao == 'c':
         terminal.socket.connect('http://localhost:3000')
         while True:
