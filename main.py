b69bdc424b5e26833d9934e23c5f5d6c588f8884
Gustavo Leão Nogueira de Oliveira <gus.leaono@gmail.com>
2021-07-14 01:09:44 -0300

    cliente criado

---
+++ main.py
@@ -0,0 +1,57 @@
+from socket import socket
+
+import socketio as sio
+
+socket = sio.Client()
+
+@socket.event
+def connect():
+    print()
+    print('Estamos conectados!')
+    print()
+
+@socket.event
+def disconnect():
+    print()
+    print('Estamos desconectados!')
+    print()
+
+@socket.event
+def error(erro):
+    print()
+    print(f'Ocorreu um erro {erro}')
+    print()
+
+@socket.on('previousMessages')
+def previousMessages(mensagens):
+    print()
+    print('Mensagens:')
+    for mensagem in mensagens:
+        print(mensagem)
+    print()
+
+@socket.on('recivedMessage')
+def recivedMessage(mensagem):
+    print()
+    print('Nova mensagem:')
+    print(mensagem)
+    print()
+
+@socket.event
+def sendMessage(mensagem):
+    print()
+    print(f'Enviando mensagem: {mensagem}')
+    print()
+    socket.emit('sendMessage', mensagem)
+
+if __name__ == '__main__':
+    socket.connect('http://localhost:3000')
+    while True:
+        usr = input('Digite seu nome:')
+        msg = input('Digite sua mensagem:')
+        mensagem={
+            'usuario': usr,
+            'mensagem': msg
+        }
+        sendMessage(mensagem)
+    socket.wait()
